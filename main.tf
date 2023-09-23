1c333eaab1af632192647cc61b3402e4b86695df
Robert Yamasaki <rhy2781@rit.edu>
2023-09-23 12:08:42 -0400

    created output bucket for athena, and shifted credentials into external tfvars file for ease of use

--- main.tf
+++ main.tf
@@ -1,7 +1,24 @@
-provider "aws" {}
-
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+variable "aws_region"{
+  type = string
 }
 
+variable "aws_access_key"{
+  type = string
+}
+
+variable "aws_secret_key"{
+  type = string
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_s3_bucket" "rito-athena-output"{
+  tags = {
+     power-rankings-hackathon = 2023
+  }
+}
 
