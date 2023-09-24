bc193d56fddf7ca85c5a35d34aad678950b11bbe
Robert Yamasaki <rhy2781@rit.edu>
2023-09-24 16:38:29 -0400

    added security group for ec2 ssh connection:wq

--- main.tf
+++ main.tf
@@ -28,12 +28,33 @@ resource "aws_glue_catalog_database" "glue-db"{
   name = "rito-glue-database"
 }
 
-resource "aws_glue_crawler" "crawler"{
+resource "aws_glue_crawler" "glue-crawler"{
   database_name = aws_glue_catalog_database.glue-db.name
   name = "rito-glue-crawler"
   role = "arn:aws:iam::782420675586:role/service-role/AWSGlueServiceRole-test"
   
   s3_target{
     path = "s3://power-rankings-dataset-gprhack/athena-ready/"
+  }
+}
+
+resource "aws_security_group" "ec2-ssh"{
+  name_prefix = "ec2-ssh-"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource  "aws_instance" "ec2"{
+  ami = "ami-03a6eaae9938c858c"
+  instance_type = "t2.micro"
+  key_name = "my-AWS-key"
+  vpc_security_group_ids = [aws_security_group.ec2-ssh.id]
+  tags = {
+    name = "backend-ec2"
+    power-rankings-hackathon = 2023
   }
 }
